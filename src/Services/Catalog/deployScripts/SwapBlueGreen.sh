5a4a24d386e999e7cafc19440f4ec50eb17838c0
Nicolas Yuen <niyuen@microsoft.com>
2019-04-09 23:51:08 +0200

    removing pipeline variable

--- src/Services/Catalog/deployScripts/SwapBlueGreen.sh
+++ src/Services/Catalog/deployScripts/SwapBlueGreen.sh
@@ -3,8 +3,11 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-if [ $(green_enabled) = true ]; then
+#if [ "$(green_enabled)" = true ]; then
 
+productionSlot=$( (
+        helm get values --all eshoponcontainers-aks-catalog-api | grep 'productionSlot: ' | awk -F': ' '{print $2}'
+    ))
     #the variables has been set in the Azure pipeline during the first stage of the release
     echo -e "${RED}new Release version = ${$(newRelease)}${NC}"
     echo -e "${BLUE}blue Release version = ${$(blueRelease)}${NC}"
@@ -12,7 +15,7 @@ if [ $(green_enabled) = true ]; then
     echo -e "New version is on the $(newVersion) slot"
 
     #Swap the labels used by the service, effectively switching between the blue and the green deployment
-    if [ "$(productionSlot)" = "blue" ]; then
+    if [ "$productionSlot" = "blue" ]; then
         productionSlot="green"
         stagingSlot="blue"
     else
@@ -33,7 +36,7 @@ if [ $(green_enabled) = true ]; then
     echo "##vso[task.setvariable variable=green_enabled]$green_enabled"
 
     helm upgrade --install  --reuse-values --set version.productionSlot=$productionSlot --set version.stagingSlot=$stagingSlot --set version.green_enabled=$green_enabled $(app_name)-catalog-api "$(System.DefaultWorkingDirectory)/_specialK-CI-CatalogAPI/helm/catalog-api/k8s/helm/catalog-api"
-else
-    echo "Green Deployment is not enabled"
-    echo "Skipping tasks"
-fi
+# else
+#     echo "Green Deployment is not enabled"
+#     echo "Skipping tasks"
+# fi
